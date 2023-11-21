46b95d237cb51eb4b71ed851924caaa657ece55a
Brennen Segura <brennen@seguramgmt.com>
2023-11-21 03:23:16 -0600

    Your commit message

--- contracts/HighYieldPool.sol
+++ contracts/HighYieldPool.sol
@@ -4,6 +4,60 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./PoolContract.sol";
 
-abstract contract HighYieldPool is PoolContract {
-    // Your pool-specific logic here
+contract HighYieldPool is PoolContract {
+    IERC20 public liquidityToken;
+    IERC20 public rewardToken;
+
+    uint256 public feeRate; // Fee rate for liquidity provision
+    uint256 public totalLiquidity;
+    mapping(address => uint256) public userLiquidity;
+
+    // Events for tracking pool interactions
+    event LiquidityProvided(address indexed user, uint256 amount);
+    event LiquidityWithdrawn(address indexed user, uint256 amount);
+    event YieldDistributed(address indexed user, uint256 yield);
+
+    constructor(IERC20 _liquidityToken, IERC20 _rewardToken, uint256 _initialFeeRate) {
+        liquidityToken = _liquidityToken;
+        rewardToken = _rewardToken;
+        feeRate = _initialFeeRate;
+    }
+
+    function provideLiquidity(uint256 amount) external override {
+        require(amount > 0, "Amount must be greater than 0");
+        liquidityToken.transferFrom(msg.sender, address(this), amount);
+
+        uint256 fee = calculateFee(amount);
+        uint256 netAmount = amount - fee;
+        userLiquidity[msg.sender] += netAmount;
+        totalLiquidity += netAmount;
+
+        distributeYield(fee); // Distribute yield based on fee collected
+        emit LiquidityProvided(msg.sender, netAmount);
+    }
+
+    function withdrawLiquidity(uint256 amount) external override {
+        require(userLiquidity[msg.sender] >= amount, "Insufficient liquidity balance");
+        userLiquidity[msg.sender] -= amount;
+        totalLiquidity -= amount;
+
+        liquidityToken.transfer(msg.sender, amount);
+        emit LiquidityWithdrawn(msg.sender, amount);
+    }
+
+    function calculateFee(uint256 amount) public view returns (uint256) {
+        return (amount * feeRate) / 1e4; // Example fee calculation
+    }
+
+    function distributeYield(uint256 feeAmount) private {
+        // Logic to distribute the yield (collected fees) as rewards
+        // This could involve distributing reward tokens to liquidity providers
+    }
+
+    function calculateYield() public view returns (uint256) {
+        // Implement a logic to calculate yield based on current pool performance
+        // This might involve complex calculations depending on pool strategies
+    }
+
+    // Additional overridden functions from PoolContract as needed...
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,26 @@
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
+const YOUR_PRIVATE_KEY = process.env.PRIVATE_KEY;
+const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY;
+
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.21",
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
+  solidity: "0.8.20",
   networks: {
-    hardhat: {
-      // Configuration for the Hardhat network
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      accounts: [`0x${YOUR_PRIVATE_KEY}`],
+      chainId: 56,
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: [`0x${YOUR_PRIVATE_KEY}`],
+      chainId: 97,
     }
-    // Additional network configurations (e.g., Rinkeby, Mainnet)
+  },
+  bscscan: {
+    apiKey: BSCSCAN_API_KEY
   }
 };
